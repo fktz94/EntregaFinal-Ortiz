b0ac9080015e6d971a3260743c0c5559a90eada5
fktz94 <facundo.ortiz@live.com>
2023-07-15 17:14:58 -0300

    add hover styles to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { BiSolidDownArrow, BiSolidUpArrow } from 'react-icons/bi';
 import CartWidget from './CartWidget';
 
 function DropdownItems({ text }) {
-  const className = 'w-full p-1 block hover:bg-first';
+  const className = 'w-full p-3 block tracking-wide font-semibold hover:bg-first hover:text-fourth';
   return (
     <li className="relative">
       <a href="/" className={className}>
@@ -14,10 +14,11 @@ function DropdownItems({ text }) {
 }
 function Dropdown() {
   const [isOpen, setIsOpen] = useState(false);
-  const className = 'p-2 flex items-center gap-2 border border-transparent font-semibold text-lg';
+  const className = `p-2 flex items-center gap-2 border border-transparent font-semibold text-lg transition-colors ${
+    isOpen ? 'text-fourth' : ''
+  }`;
 
-  const handleSetIsOpen = (event) => {
-    const { type } = event;
+  const handleSetIsOpen = ({ type }) => {
     if (type === 'mouseenter') return setIsOpen(true);
     return setIsOpen(false);
   };
@@ -32,15 +33,15 @@ function Dropdown() {
         {/* Productos {isOpen ? '↑' : '↓'} */}
         Productos
         {isOpen ? (
-          <BiSolidUpArrow style={{ fontSize: '.6rem' }} />
+          <BiSolidUpArrow style={{ fontSize: '.5rem' }} />
         ) : (
-          <BiSolidDownArrow style={{ fontSize: '.6rem' }} />
+          <BiSolidDownArrow style={{ fontSize: '.5rem' }} />
         )}
       </button>
       {isOpen && (
         <div className="absolute w-full bg-third">
           <ul
-            className="border"
+            className="border border-black"
             onMouseEnter={(event) => handleSetIsOpen(event)}
             onMouseLeave={(event) => handleSetIsOpen(event)}>
             <DropdownItems text="Remeras" />
@@ -54,7 +55,8 @@ function Dropdown() {
   );
 }
 function NavLink({ text }) {
-  const className = 'p-2 border border-transparent font-semibold text-lg hover:border-b-black';
+  const className =
+    'p-2 border border-transparent font-semibold text-lg transition-colors hover:text-fourth hover:border-b-fourth';
   return (
     <li>
       <a href="/" className={`block h-full ${className}`}>
@@ -67,7 +69,9 @@ function NavLink({ text }) {
 export default function NavBar() {
   return (
     <header className="px-8 py-5 flex justify-between items-center sticky top-0 shadow-md bg-first">
-      <span className="font-bold cursor-default text-3xl tracking-wide">MORO</span>
+      <span className="font-bold cursor-default text-3xl tracking-wide transition-colors hover:text-fourth">
+        MORO
+      </span>
       <nav>
         <ul className="flex justify-between gap-2">
           <NavLink text="Nosotros" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
-    colors: {
-      black: '#393646',
-      first: '#8D7B68',
-      second: '#A4907C',
-      third: '#C8B6A6',
-      fourth: '#F1DEC9',
-      transparent: 'transparent'
+    extend: {
+      colors: {
+        black: '#393646',
+        first: '#8D7B68',
+        second: '#A4907C',
+        third: '#C8B6A6',
+        fourth: '#F1DEC9'
+      }
     }
   },
   plugins: []
