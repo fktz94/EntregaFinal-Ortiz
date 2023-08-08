e25a090d3d197d7aee4116e3970399ae0bf67d12
fktz94 <facundo.ortiz@live.com>
2023-08-08 19:05:54 -0300

    not found style

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,9 +2,13 @@ import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="flex flex-col items-center gap-2">
       <h1>page not found!</h1>
-      <Link to=".">Volver al home!</Link>
+      <Link
+        to="."
+        className="px-5 py-2 text-xl font-semibold rounded shadow border border-transparent bg-first hover:bg-second hover:border-black active:text-light">
+        Volver al home
+      </Link>
     </div>
   );
 }
